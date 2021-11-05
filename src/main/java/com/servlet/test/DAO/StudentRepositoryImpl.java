261780271b172df62e4ee45ac6a2e3f5b7506ffc
serhiyb86 <79664086+serhiyb86@users.noreply.github.com>
2021-11-05 17:48:50 +0200

    fixed

--- src/main/java/com/servlet/test/DAO/StudentRepositoryImpl.java
+++ src/main/java/com/servlet/test/DAO/StudentRepositoryImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class StudentRepositoryImpl implements StudentRepository {
 
-    private final String INSERT_STUDENT = "INSERT INTO `students`(`LastName`, `FirstName`, `Age`, `course`) " +
+    private final String INSERT_STUDENT = "INSERT INTO `students`(`age`, `course`, `first_name`, `last_name`) " +
             "VALUES (?,?,?,?)";
     private final String SELECT_ALL = "SELECT * FROM `students` ORDER by id";
 
@@ -26,10 +26,10 @@ public class StudentRepositoryImpl implements StudentRepository {
             ResultSet rs = statement.executeQuery(SELECT_ALL);
             while (rs.next()) {
                 Student student = new Student();
-                student.setFirstName(rs.getString(3));
-                student.setLastName(rs.getString(2));
-                student.setCourse(Courses.valueOf(rs.getString(5)));
-                student.setAge(rs.getInt(4));
+                student.setFirstName(rs.getString(4));
+                student.setLastName(rs.getString(5));
+                student.setCourse(Courses.values()[rs.getInt(3)]);
+                student.setAge(rs.getInt(2));
                 student.setId(rs.getInt(1));
                 students.add(student);
             }
@@ -42,10 +42,10 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public Student addStudent(Student student) {
         try (PreparedStatement pstmt = connection.prepareStatement(INSERT_STUDENT)) {
-            pstmt.setString(1, student.getLastName());
-            pstmt.setString(2, student.getFirstName());
-            pstmt.setInt(3, student.getAge());
-            pstmt.setString(4, student.getCourse().name());
+            pstmt.setString(4, student.getLastName());
+            pstmt.setString(3, student.getFirstName());
+            pstmt.setInt(1, student.getAge());
+            pstmt.setInt(2, student.getCourse().ordinal());
             pstmt.execute();
         } catch (SQLException e) {
             e.printStackTrace();
