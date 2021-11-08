7c703bea340370e36e575202b001290974abc78f
Serhii Bohush <serhiyb86@gmail.com>
2021-11-08 23:07:30 +0200

    test 5 ant

--- src/main/java/com/servlet/test/ApplicationStarter.java
+++ src/main/java/com/servlet/test/ApplicationStarter.java
@@ -2,9 +2,10 @@ package com.servlet.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationStarter {
+public class ApplicationStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
     }
--- target/servletTask/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<h2>It's a simple application!</h2>
-<p><a href="/add">ADD Student to a particular course.</a> </p>
-<p></p>
-<p><a href="/allStudents">List students</a> </p>
-</body>
-</html>
